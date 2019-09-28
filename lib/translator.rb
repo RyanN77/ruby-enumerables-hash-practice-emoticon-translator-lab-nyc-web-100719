970167c57c2006fd8a7d726127dd1f01b8f76e58
adventurous-class-0665 <rngao97@gmail.com>
2019-09-28 18:18:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,9 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   hash = load_library(file_path)
   hash[:get_emoticon].each do |key, value|
-    print key == emoticon
+    if key == emoticon
+      return value
+    end
   end
   return emoticon
 end
