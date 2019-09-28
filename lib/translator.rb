2d32a22868de39b0d469e79b22d63957d1f2ca07
adventurous-class-0665 <rngao97@gmail.com>
2019-09-28 17:47:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file)
   YAML.load_file(file).each do |key, value|
     print key
     hash[:get_meaning][value[1]] = key
-    hash[:get_emoticon][value[0]] = hash[:get_meaning][value[1]]
+    hash[:get_emoticon][value[0]] = hash[:get_meaning][value[1]][key]
 
     
     end
